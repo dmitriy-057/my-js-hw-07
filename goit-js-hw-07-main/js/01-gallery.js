bba8665e3dd657580fb35427c6d956625b8c00a9
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-29 23:30:44 +0200

    add gallery-01

--- goit-js-hw-07-main/js/01-gallery.js
+++ goit-js-hw-07-main/js/01-gallery.js
@@ -1,3 +1,45 @@
 import { galleryItems } from "./gallery-items.js";
 
+const divEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+console.log(galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    }).join('');
+};
+
+divEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+divEl.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    if(evt.target.nodeName !== 'IMG') {  
+        return;
+    };
+
+    console.log('target', evt.target);
+    const instance = basicLightbox.create(` 
+    <img src="${evt.target.dataset.source}">
+    `)
+    
+    instance.show();
+};
+
+
+
+
 
