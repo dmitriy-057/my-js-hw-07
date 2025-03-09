962ddb39da62e77a057ec51baeb89b8a0dd02d3c
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-09 23:17:34 +0200

    add files

--- goit-js-hw-07-main/js/02-lightbox.js
+++ goit-js-hw-07-main/js/02-lightbox.js
@@ -1,39 +1,3 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
-
-const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-
-function createGalleryItemsMarkup(items) {
-    return items.map(({preview, original, description}) => {
-       return `<div class="gallery__item">
-        <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-      </a>
-      </div>`
-    }).join('');
-}
-
-galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-
-galleryEl.addEventListener('click', onModalOpen);
-
-function onModalOpen(evt) {
-    evt.preventDefault();
-    
-    if(evt.target.nodeName !== "IMG") {
-        return;
-    };
-
-    addSimpleLightBox();
-};
-
-function addSimpleLightBox() {
-    const lightbox = new SimpleLightbox('.gallery__item', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-        enableKeyboard: true,
-    });
-}
